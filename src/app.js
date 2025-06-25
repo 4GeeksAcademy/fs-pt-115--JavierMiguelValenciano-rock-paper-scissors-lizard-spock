9783ad4b525e36c68d5a3eae746c4862a560d9a3
Jmvdev <jvi96miguel@gmail.com>
2025-06-25 19:23:11 +0000

    updated v2 simplify switch and more readable

--- src/app.js
+++ src/app.js
@@ -1,68 +1,59 @@
 //write your code here
 
-const hands = ["rock","paper","scissors","lizard","spock"];
+const hands = ["rock", "paper", "scissors", "lizard", "spock"];
 
 // Computer choice function
-const  ComputerChoice = () => {
+const ComputerChoice = () => {
     return hands[Math.floor(Math.random() * hands.length)];
 }
-// console.log(ComputerChoice());
 
 //Check rules and winer
- const CheckingWinner = (humanChoice) =>
- {
+const CheckingWinner = (humanChoice) => {
     let computer = ComputerChoice();
+
+    let result = "";
 
     console.log("You choose: " + humanChoice);
     console.log("Computer say: " + computer);
 
-    switch (humanChoice) {
-        case "rock":
-
-            (computer == "scissors" || computer == "lizard") ? console.log("Congrats you're the winner !!") : (computer == "spock" || computer == "paper") ? console.log("Sorry looser !! try again !!") : console.log("This is a well-played draw !!");
+    switch (true) {
+        case humanChoice === computer:
+            result = "This is a well-played draw !!";
             break;
 
-        case "paper":
-
-            (computer == "rock" || computer == "spock") ? console.log("Congrats you're the winner !!") : (computer == "scissors" || computer == "lizard") ? console.log("Sorry looser !! try again !!") : console.log("This is a well-played draw !!");
+        case humanChoice === "rock" && (computer === "scissors" || computer === "lizard"):
+        case humanChoice === "paper" && (computer === "rock" || computer === "spock"):
+        case humanChoice === "scissors" && (computer === "paper" || computer === "lizard"):
+        case humanChoice === "lizard" && (computer === "paper" || computer === "spock"):
+        case humanChoice === "spock" && (computer === "scissors" || computer === "rock"):
+            result = "Congrats you're the winner !!";
             break;
 
-        case "scissors":
-
-            (computer == "paper" || computer == "lizard") ? console.log("Congrats you're the winner !!") : (computer == "rock" || computer == "spock") ? console.log("Sorry looser !! try again !!") : console.log("This is a well-played draw !!");
+        default:
+            result = "Sorry loser !! Try again !!";
             break;
+    }
 
-        case "lizard":
+    return result;
 
-            (computer == "paper" || computer == "spock") ? console.log("Congrats you're the winner !!") : (computer == "rock" || computer == "scissors") ? console.log("Sorry looser !! try again !!") : console.log("This is a well-played draw !!");
-            break;
-
-        case "spock":
-
-            (computer == "scissors" || computer == "rock") ? console.log("Congrats you're the winner !!") : (computer == "spock" || computer == "lizard") ? console.log("Sorry looser !! try again !!") : console.log("This is a well-played draw !!");
-            break;
 }
-    
- }
 
 // Start Game
 let userChoice = null;
 
 let rightChoice = false;
 
-while (rightChoice == false)
-{
+while (rightChoice == false) {
     userChoice = prompt("Choose between: rock, paper, scissors, lizard or spock.");
 
-     if(userChoice != "rock" && userChoice != "paper" && userChoice != "scissors" && userChoice != "lizard" && userChoice != "spock")
-     {
-         console.log("It's your choice, play or get out !!, this is a serious game.");
-     }
-     else
-     {
+    // Right Conditions check
+    if (userChoice != "rock" && userChoice != "paper" && userChoice != "scissors" && userChoice != "lizard" && userChoice != "spock") {
+        console.log("It's your choice, play or get out !!, this is a serious game.");
+    }
+    else {
         console.log("nice choice Good Luck");
         rightChoice = true;
     }
 }
 
-CheckingWinner(userChoice);
+console.log(CheckingWinner(userChoice));
